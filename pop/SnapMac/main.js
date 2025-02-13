dd3910f70e232c34cdff6ca8782c7313d0ad0720
Max Dawson <maxjdawson7@gmail.com>
2025-02-13 21:30:44 +0000

    code

---
+++ pop/SnapMac/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+function createWindow() {
+  // Create the browser window
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  // Set custom User-Agent to bypass "browser not supported"
+  const customUserAgent =
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36";
+
+  // Load the website instead of a local file
+  mainWindow.loadURL("https://web.snapchat.com", {
+    userAgent: customUserAgent
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
